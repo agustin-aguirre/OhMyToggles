0d42e3c5788720ea6952c3e91e83c22797aaabbb
Agustín Aguirre <agus.aguirre.pro@gmail.com>
2025-06-05 11:43:19 -0300

    Moved implementations of toggles from examples to runtime

---
+++ Runtime/ColorTransitionToggle.cs
@@ -0,0 +1,62 @@
+﻿using OhMyToggles.Interfaces;
+using OhMyToggles.Transitions;
+using OhMyToggles.Themes;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+namespace OhMyToggles
+{
+	public class ColorTransitionToggle : PointerInteractableOption
+	{
+		public Image TargetGraphic;
+
+		public ColorTheme Theme;
+
+		[HideInInspector, SerializeField] ColorTransition transition;
+
+
+		protected override void Awake()
+		{
+			base.Awake();
+			ApplyTheme();
+		}
+
+
+#if UNITY_EDITOR
+		[ContextMenu("Apply Theme")]
+#endif
+		public void ApplyTheme()
+		{
+			transition = new ColorTransition(this, TargetGraphic);
+
+			if (Theme != null)
+			{
+				transition.UseTheme(Theme);
+			}
+
+			ForceUpdateVisualsToPointerRelativeState(State);
+		}
+
+
+		public override void ForceUpdateVisualsToPointerRelativeState(PointerRelativeState pointerRelativeState) => transition.ForceUpdateVisualsToPointerRelativeState(pointerRelativeState);
+
+
+
+#if UNITY_EDITOR
+		[ContextMenu("Turn On")]
+		public void TurnOn()
+		{
+			ForceIsOnValue(false);
+			Select(true);
+		}
+		
+		[ContextMenu("Turn Off")]
+		public void TurnOff()
+		{
+			ForceIsOnValue(true);
+			Select(false);
+		}
+#endif
+	}
+}
